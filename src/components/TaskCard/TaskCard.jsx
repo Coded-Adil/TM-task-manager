f14c4d4d474b1c8ae1aaf50ecd0ec2dd7f9070f9
Adil Rahman <itsadil2022@gmail.com>
2025-01-04 17:52:56 +0600

    Added some hover effect cards

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,18 +1,17 @@
-
 const TaskCard = ({ task }) => {
     const { title, description, type, deadline, created_at } = task;
     return (
-        <div className="p-6 border-2 rounded-lg">
+        <div className="p-6 rounded-lg shadow-lg text-white transition-transform transform hover:scale-105 hover:shadow-2xl hover:shadow-black">
             <div className="flex flex-col gap-2">
-                <h2>{title}</h2>
-                <p>{description}</p>
+                <h2 className="text-xl font-semibold">{title}</h2>
+                <p className="text-white">{description}</p>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between mt-4 text-sm">
                 <div>
-                    <h2>{deadline}</h2>
-                    <h2>{created_at}</h2>
+                    <h2>Deadline: {deadline}</h2>
+                    <h2>Created: {created_at}</h2>
                 </div>
-                <p>{type}</p>
+                <p className="font-bold">{type}</p>
             </div>
         </div>
     );
