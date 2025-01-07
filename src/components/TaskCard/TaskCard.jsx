1acc20b14218f630462bde64a06a5dfb261a2bad
Adil Rahman <itsadil2022@gmail.com>
2025-01-07 19:57:50 +0600

    fix cards

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,21 +1,28 @@
 const TaskCard = ({ task }) => {
-    const { title, description, type, deadline, created_at, assigned_to, created_by } = task;
+    const { title, type, deadline, assigned_to } = task;
     return (
         <div className="p-6 rounded-lg shadow-lg text-white transition-transform transform duration-300 hover:scale-105 hover:shadow-2xl hover:shadow-black">
             <div className="flex flex-col gap-2">
                 <h2 className="text-xl font-semibold">{title}</h2>
-                <p className="text-white">{description}</p>
             </div>
             <div className="flex justify-between mt-4 text-sm">
-                <div>
+                <div className="my-2">
                     <h2>Deadline: {deadline}</h2>
-                    <h2>Created: {created_at}</h2>
                 </div>
-                <p className="font-bold">{type}</p>
+                <p className="font-bold text-blue-500">{type}</p>
             </div>
-            <div>
-                <h2>Assigned To: {assigned_to.name}</h2>
-                <h2>Assigned By: {created_by.name}</h2>
+            <div className="space-y-2">
+                <h2 className="font-bold">Assigned To: </h2>
+                <ul className="flex justify-start items-center gap-2">
+                    <li>
+                        <img
+                            className="h-10 w-10 rounded-full border border-gray-700"
+                            src={assigned_to.photoURL}
+                            alt="User Avatar"
+                        />
+                    </li>
+                    <li>{assigned_to.name}</li>
+                </ul>
             </div>
         </div>
     );
